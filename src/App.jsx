92367c1de2fda4f7d51a5ae963201bb483bf1618
michelangelomancini <michelangelomancini8@gmail.com>
2025-06-19 14:04:04 +0200

    Milestone 1 completed -created form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fullName, setFullName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [specialization, setSpecialization] = useState('');
+  const [experience, setExperience] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      !fullName.trim() ||
+      !username.trim() ||
+      !password.trim() ||
+      !specialization.trim() ||
+      !experience.trim() ||
+      experience <= 0 ||
+      !description.trim()
+    ) {
+      alert('Per favore compila tutti i campi correttamente.');
+      return;
+    }
+    const formData = {
+      fullName,
+      username,
+      password,
+      specialization,
+      experience,
+      description,
+    };
+
+
+    console.log('Dati utente registrato:', formData);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div >
+      <h1>Registrazione</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome completo:
+          <input
+            type="text"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+
+          />
+        </label>
+
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Specializzazione:
+          <select
+            value={specialization}
+            onChange={(e) => setSpecialization(e.target.value)}
+          >
+            <option value="Full Stack">Full Stack</option>
+            <option value="Frontend">Frontend</option>
+            <option value="Backend">Backend</option>
+          </select>
+        </label>
+
+        <label>
+          Anni di esperienza:
+          <input
+            type="number"
+            value={experience}
+            onChange={(e) => setExperience(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Descrizione:
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+
+        <button type="submit">Registrati</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
