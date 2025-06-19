f96ebe03a53774ac7e6d60ea695d828297269817
michelangelomancini <michelangelomancini8@gmail.com>
2025-06-19 16:19:04 +0200

    Milestone 2 completed -  live validation for username, password and description

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
-import { useState } from 'react';
+// Aggiungere la validazione in tempo reale dei seguenti campi:
+
+// ✅ Username: Deve contenere solo caratteri alfanumerici e almeno 6 caratteri (no spazi o simboli).
+
+// ✅ Password: Deve contenere almeno 8 caratteri, 1 lettera, 1 numero e 1 simbolo.
+
+// ✅ Descrizione: Deve contenere tra 100 e 1000 caratteri (senza spazi iniziali e finali).
+
+// Suggerimento: Per semplificare la validazione, puoi definire tre stringhe con i caratteri validi e
+//  usare .includes() per controllare se i caratteri appartengono a una di queste categorie:
+
+
+import { useMemo, useState } from 'react';
 
 function App() {
   const [fullName, setFullName] = useState('');
@@ -7,6 +19,34 @@ function App() {
   const [specialization, setSpecialization] = useState('');
   const [experience, setExperience] = useState('');
   const [description, setDescription] = useState('');
+
+
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "!@#$%^&*()-_=+[]{}|;:'\\,.<>?/`~";
+
+  const isUsernameValid = useMemo(() => {
+
+    const validUsername = username.split("").every(char => letters.includes(char.toLowerCase()) || numbers.includes(char))
+    return validUsername && username.length >= 6;
+
+  }, [username]);
+
+
+  const isPasswordValid = useMemo(() => {
+    return password.length >= 6 && password.split("").some(char => letters.includes(char)) &&
+      password.split("").some(char => numbers.includes(char)) &&
+      password.split("").some(char => symbols.includes(char))
+
+
+  }, [password]);
+
+  const isDescriptionValid = useMemo(() => {
+
+    return description.trim().length >= 10 &&
+      description.trim().length <= 1000
+  }, [description])
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -57,7 +97,18 @@ function App() {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
+          {username && !isUsernameValid && (
+            <p className="invalid">
+              Solo lettere e numeri, almeno 6 caratteri.
+            </p>
+          )}
+          {username && isUsernameValid && (
+            <p className="valid">
+              Username valido
+            </p>
+          )}
         </label>
+
 
         <label>
           Password:
@@ -66,6 +117,16 @@ function App() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {password && !isPasswordValid && (
+            <p className="invalid">
+              Almeno 6 caratteri, 1 lettera, 1 numero, 1 simbolo.
+            </p>
+          )}
+          {password && isPasswordValid && (
+            <p className="valid">
+              Password valida
+            </p>
+          )}
         </label>
 
         <label>
@@ -95,6 +156,16 @@ function App() {
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
+          {description && !isDescriptionValid && (
+            <p className="invalid">
+              Deve contenere tra 100 e 1000 caratteri.
+            </p>
+          )}
+          {description && isDescriptionValid && (
+            <p className="valid">
+              Descrizione valida
+            </p>
+          )}
         </label>
 
         <button type="submit">Registrati</button>
